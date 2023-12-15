2711a65cbd546171042b757876db36cb4e8975a7
pjx19 <pjx1572@gmail.com>
2023-12-15 13:59:18 +0100

    ervaringsdeskundige portaal gemaakt

---
+++ accessibilitywebapp.client/src/ErvaringsdeskundigePortaal.jsx
@@ -0,0 +1,59 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./ErvaringsdeskundigePortaal.css";
+
+export const Ervaringsdeskundige = () => {
+    return (
+        <div className="ervaringsdeskundige">
+            <div className="div">
+                <div className="overlap">
+                    <div className="overlap-group">
+                        <div className="rectangle" />
+                        <div className="text-wrapper">Log uit</div>
+                        <div className="rectangle-2" />
+                        <div className="text-wrapper-2">Home</div>
+                        <div className="text-wrapper-3">Profiel</div>
+                        <img className="line" alt="Line" src="line-1.svg" />
+                        <img className="image" alt="Image" src="image-9.png" />
+                        <div className="ellipse" />
+                    </div>
+                    <div className="text-wrapper-4">Welkom Jan</div>
+                </div>
+                <div className="text-wrapper-5">Uw onderzoeken:</div>
+                <div className="overlap-group-2">
+                    <div className="text-wrapper-6">Onderzoek KPN</div>
+                    <div className="text-wrapper-7">Onderzoek ING</div>
+                    <div className="rectangle-3" />
+                    <div className="text-wrapper-8">Onderzoek KPN</div>
+                    <div className="rectangle-4" />
+                    <div className="text-wrapper-9">Onderzoek ING</div>
+                    <div className="text-wrapper-10">Onderzoek ING</div>
+                    <div className="rectangle-5" />
+                    <div className="rectangle-6" />
+                    <div className="text-wrapper-11">Onderzoek Hema</div>
+                </div>
+                <div className="rectangle-wrapper">
+                    <div className="rectangle-7" />
+                </div>
+                <div className="rectangle-8" />
+                <div className="overlap-2">
+                    <div className="text-wrapper-12">Onderzoek KPN</div>
+                    <div className="text-wrapper-13">Onderzoek ING</div>
+                    <div className="rectangle-9" />
+                    <div className="text-wrapper-14">Onderzoek KPN</div>
+                    <div className="rectangle-10" />
+                    <div className="text-wrapper-15">Onderzoek ING</div>
+                    <div className="text-wrapper-16">Onderzoek ING</div>
+                    <div className="rectangle-11" />
+                    <div className="rectangle-12" />
+                    <div className="text-wrapper-17">Onderzoek Hema</div>
+                </div>
+                <div className="div-wrapper">
+                    <div className="rectangle-7" />
+                </div>
+                <div className="rectangle-13" />
+                <div className="text-wrapper-18">Uw voltooide onderzoeken:</div>
+            </div>
+        </div>
+    );
+};
