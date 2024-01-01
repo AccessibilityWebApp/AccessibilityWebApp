f3d19e827b2bc4ad98d96373b7bf6e00358f2018
IsoChan070 <22115862@student.hhs.nl>
2024-01-01 18:02:18 +0100

    expert profiel toegevoegd

---
+++ accessibilitywebapp.client/src/pages/screens/ExpertProfiel.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import "./style.css";
+
+export const Profiel = () => {
+    return (
+        <div className="profiel">
+            <div className="div">
+                <div className="overlap">
+                    <div className="rectangle" />
+                    <div className="text-wrapper">IsoCan070</div>
+                    <img className="vector" alt="Vector" src="vector-15.svg" />
+                </div>
+                <div className="overlap-group">
+                    <img className="img" alt="Rectangle" src="rectangle-53.svg" />
+                </div>
+                <div className="overlap-2">
+                    <div className="rectangle-2" />
+                    <div className="text-wrapper-2">Verwijder account</div>
+                    <div className="group">
+                        <div className="div-wrapper">
+                            <div className="text-wrapper-3">Opslaan</div>
+                        </div>
+                    </div>
+                    <p className="voorletter-m">
+                        Voorletter: M<br />
+                        Achternaam: Gunus
+                        <br />
+                        leeftijdsgroep: 18-35
+                        <br />
+                        Postcode: 4201GS
+                        <br />
+                        E-mail: BabaAhmet@gmail.com
+                        <br />
+                        Telefoonnummer: 06 12345678
+                    </p>
+                    <div className="group-2">
+                        <div className="div-2">
+                            <div className="text-wrapper-4">M.</div>
+                        </div>
+                        <div className="overlap-3">
+                            <div className="text-wrapper-5">4206GG</div>
+                        </div>
+                        <div className="overlap-4">
+                            <div className="text-wrapper-6">MGunus@gmail.com</div>
+                        </div>
+                        <div className="overlap-5">
+                            <div className="div-2" />
+                            <div className="text-wrapper-7">18-35</div>
+                        </div>
+                        <div className="overlap-6">
+                            <div className="text-wrapper-8">Gunus</div>
+                        </div>
+                        <div className="overlap-7">
+                            <div className="div-2" />
+                            <div className="text-wrapper-9">06 12345678</div>
+                        </div>
+                        <div className="overlap-8">
+                            <div className="div-2" />
+                            <div className="text-wrapper-10">Doof</div>
+                        </div>
+                        <div className="overlap-9">
+                            <div className="div-2" />
+                            <div className="text-wrapper-11">Telefonisch</div>
+                        </div>
+                        <div className="overlap-10">
+                            <div className="text-wrapper-12">Online</div>
+                        </div>
+                    </div>
+                    <div className="text-wrapper-13">Wachtwoord wijzigen</div>
+                </div>
+                <div className="overlap-11">
+                    <div className="text-wrapper-14">Mijn gegevens</div>
+                    <img className="vector-2" alt="Vector" src="vector-14.svg" />
+                </div>
+                <div className="overlap-12">
+                    <img className="vector-3" alt="Vector" src="vector-16.svg" />
+                    <img className="image" alt="Image" src="image-8.png" />
+                </div>
+                <div className="overlap-13">
+                    <div className="text-wrapper-15">Log uit</div>
+                    <div className="text-wrapper-15">Log uit</div>
+                </div>
+                <div className="overlap-14">
+                    <div className="text-wrapper-16">Home</div>
+                    <div className="text-wrapper-16">Home</div>
+                </div>
+                <div className="overlap-15">
+                    <div className="text-wrapper-17">Profiel</div>
+                    <div className="rectangle-3" />
+                    <div className="text-wrapper-18">Profiel</div>
+                </div>
+                <img className="line" alt="Line" src="line-1.svg" />
+                <img className="image-2" alt="Image" src="image-9.png" />
+                <div className="ellipse" />
+                <img className="ellipse-2" alt="Ellipse" src="ellipse-15.svg" />
+            </div>
+        </div>
+    );
+};
