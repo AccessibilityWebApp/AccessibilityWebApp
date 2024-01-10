e214be305b14c26898230ee53b72812a13c2d540
advanced03 <incedalsrf@gmail.com>
2024-01-10 21:52:26 +0100

    Login scherm verbeterd

--- accessibilitywebapp.client/src/pages/screens/ExpertLogin.jsx
+++ accessibilitywebapp.client/src/pages/screens/ExpertLogin.jsx
@@ -1,48 +1,55 @@
-import React from 'react';
-import { Container, Button, Form } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import "@/pages/pages_css/Login.css";
 
 const ExpertLogin = ({ handleToAanmeld, handleToPortal, handleToExpertPortal }) => {
-    return (
-        <Container fluid className="margin">
-            <h1 className="mb-3">Login</h1>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Gebruikersnaam / E-Mail</Form.Label>
-                    <Form.Control type="email" />
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
+    return (<body className="logIn">
+        <div id="login-form">
+            <h1>Inloggen</h1>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Form.Group controlId="username">
+                    <Form.Label>Gebruikersnaam:</Form.Label>
+                    <Form.Control type="text" name="username" required />
+                    <Form.Control.Feedback type="invalid">Vul uw gebruikersnaam in.</Form.Control.Feedback>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Wachtwoord</Form.Label>
-                    <Form.Control type="password" placeholder="" />
+                <Form.Group controlId="password">
+                    <Form.Label>Wachtwoord:</Form.Label>
+                    <Form.Control type="password" name="password" required />
+                    <Form.Control.Feedback type="invalid">Vul uw wachtwoord in.</Form.Control.Feedback>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Aangemeld blijven" />
-                </Form.Group>
+                <Form.Check type="checkbox" label="Blijf ingelogd" className="mt-3" />
 
-                <div className="d-grid gap-2">
-                    <Button variant="secondary" onClick={handleToPortal}>
-                        Back
-                    </Button>
-                    <Button variant="primary" onClick={handleToExpertPortal}>
-                        Next
-                    </Button>
-                </div>
+                <Button variant="primary" type="submit" className="w-100 mt-3">
+                    Aanmelden
+                </Button>
 
-                <div className="mt-3">
-                    <Button variant="link">
-                        Wachtwoord vergeten?
-                    </Button>
-                </div>
+                <Button variant="link" type="submit" size="sm" className="mt-2 p-0">
+                    Wachtwoord vergeten?
+                </Button>
 
-                <div className="mt-3">
-                    <Button variant="link">
-                        Heb je nog geen account? Meld je hier aan
-                    </Button>
-                </div>
+                <p className="forgotPass">Heb je nog geen account?</p>
+
+                <Button href="expert_signup" variant="success" type="submit" size="sm" className="mt-2 text-left">
+                    Registreren
+                </Button>
             </Form>
-        </Container>
+        </div>
+    </body>
     );
 };
 
