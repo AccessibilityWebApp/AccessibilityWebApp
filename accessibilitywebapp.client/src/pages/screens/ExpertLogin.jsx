23e4ac4ec6f9693a99cf8b15ce1f7aef8b48e094
advanced03 <incedalsrf@gmail.com>
2024-01-11 17:21:15 +0100

    begin gemaakt aan account aanmaak pagina

--- accessibilitywebapp.client/src/pages/screens/ExpertLogin.jsx
+++ accessibilitywebapp.client/src/pages/screens/ExpertLogin.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import { Button, Form, Row, Col } from "react-bootstrap";
+import PropTypes from "prop-types";
 import "@/pages/pages_css/Login.css";
 
-const ExpertLogin = ({ handleToAanmeld, handleToPortal, handleToExpertPortal }) => {
+const ExpertLogin = ({
+    handleToAanmeld,
+    handleToPortal,
+    handleToExpertPortal,
+}) => {
     const [validated, setValidated] = useState(false);
 
     const handleSubmit = (event) => {
@@ -16,40 +20,63 @@ const ExpertLogin = ({ handleToAanmeld, handleToPortal, handleToExpertPortal }) 
         setValidated(true);
     };
 
-    return (<body className="logIn">
-        <div id="login-form">
-            <h1>Inloggen</h1>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                <Form.Group controlId="username">
-                    <Form.Label>Gebruikersnaam:</Form.Label>
-                    <Form.Control type="text" name="username" required />
-                    <Form.Control.Feedback type="invalid">Vul uw gebruikersnaam in.</Form.Control.Feedback>
-                </Form.Group>
+    return (
+        <body className="logIn">
+            <div id="login-form">
+                <h1>Inloggen</h1>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Form.Group controlId="username">
+                        <Form.Label>Gebruikersnaam:</Form.Label>
+                        <Form.Control type="text" name="username" required />
+                        <Form.Control.Feedback type="invalid">
+                            Vul uw gebruikersnaam in.
+                        </Form.Control.Feedback>
+                    </Form.Group>
 
-                <Form.Group controlId="password">
-                    <Form.Label>Wachtwoord:</Form.Label>
-                    <Form.Control type="password" name="password" required />
-                    <Form.Control.Feedback type="invalid">Vul uw wachtwoord in.</Form.Control.Feedback>
-                </Form.Group>
+                    <Form.Group controlId="password">
+                        <Form.Label>Wachtwoord:</Form.Label>
+                        <Form.Control type="password" name="password" required />
+                        <Form.Control.Feedback type="invalid">
+                            Vul uw wachtwoord in.
+                        </Form.Control.Feedback>
+                    </Form.Group>
 
-                <Form.Check type="checkbox" label="Blijf ingelogd" className="mt-3" />
+                    <Form.Check type="checkbox" label="Blijf ingelogd" className="mt-3" />
 
-                <Button variant="primary" type="submit" className="w-100 mt-3">
-                    Aanmelden
-                </Button>
+                    <Button variant="primary" type="submit" className="w-100 mt-3">
+                        Aanmelden
+                    </Button>
 
-                <Button variant="link" type="submit" size="sm" className="mt-2 p-0">
-                    Wachtwoord vergeten?
-                </Button>
+                    <Row>
+                        <Col>
+                            <Button
+                                variant="link"
+                                type="submit"
+                                size="sm"
+                                className="mt-2 p-0"
+                            >
+                                Wachtwoord vergeten?
+                            </Button>
+                        </Col>
 
-                <p className="forgotPass">Heb je nog geen account?</p>
-
-                <Button href="expert_signup" variant="success" type="submit" size="sm" className="mt-2 text-left">
-                    Registreren
-                </Button>
-            </Form>
-        </div>
-    </body>
+                        <Col>
+                            <p className="forgotPass">Heb je nog geen account?</p>
+                        </Col>
+                        <Col>
+                            <Button
+                                href="expert_signup"
+                                variant="success"
+                                type="submit"
+                                size="sm"
+                                className="mt-2 text-left"
+                            >
+                                Registreren
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </div>
+        </body>
     );
 };
 
